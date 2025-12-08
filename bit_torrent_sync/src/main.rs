a5fde1d27fff3a754147dec138fd7819241d3562
Tushya Gupta <tushyagupta81@gmail.com>
2025-12-08 17:15:41 +0530

    diffrent workspaces for async impl(TODO)

---
+++ bit_torrent_sync/src/main.rs
@@ -0,0 +1,13 @@
+mod bencode;
+mod download;
+mod io;
+mod network;
+mod peers;
+mod tracker;
+
+use crate::download::download;
+
+fn main() {
+    download("./torrents/wired-cd.torrent".to_string())
+    // download("./torrents/test.torrent".to_string())
+}
